a67da0de01ef14764297fb6cc989b7130753f21f
anacperdigao <anacarolinaperdigao1@gmail.com>
2022-12-01 11:48:06 -0300

    feat: creating component Temporizador

---
+++ src/components/Temporizador.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="is-flex is-align-items-center is-justify-content-space-between">
+        <!-- Prestar atenção nos dois pontos antes de tempoEmSegundos pq é assim que linka com um estado -->
+        <Cronometro :tempoEmSegundos="tempoEmSegundos"/>
+        <button class="button" @click="iniciar" :disabled="cronometroRodando">
+            <span class="icon">
+                <i class="fas fa-play"></i>
+            </span>
+            <span>play</span>
+        </button>
+        <!-- Prestar atenção nos dois pontos antes de disabled pq é assim que linka com um estado -->
+        <button class="button" @click="finalizar" :disabled="!cronometroRodando">
+            <span class="icon">
+                <i class="fas fa-stop"></i>
+            </span>
+            <span>stop</span>
+        </button>
+    </div>
+</template>
+
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Cronometro from './Cronometro.vue';
+
+export default defineComponent ({
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: 'Temporizador',
+
+    //Dentro de data é onde eu vou colocar os estados que eu quero manipular
+    data () {
+        return {
+            tempoEmSegundos: 0,
+            cronometro: 0,
+            cronometroRodando: false,
+        }
+    },
+
+
+    //Dentro de methods são os métodos(funções) que eu vou utilizar nesse componente
+    methods: {
+        iniciar () {
+            // essa função nativa do JS recebe 2 params, uma função que de fato quero fazer, e o tempo em milisegundos
+            this.cronometro = setInterval(() => {
+                this.cronometroRodando = true
+                this.tempoEmSegundos = this.tempoEmSegundos + 1
+            }, 1000)
+        },
+        finalizar () {
+            this.cronometroRodando= false
+            clearInterval(this.cronometro)
+        }
+    },
+
+    components: {
+        Cronometro,
+    }
+})
+
+</script>
+
+
+<style>
+
+
+</style>
