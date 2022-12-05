4b72229f8b1c43d55ccfd5f0c85fb9f25554f150
anacperdigao <anacarolinaperdigao1@gmail.com>
2022-12-05 09:14:22 -0300

    refactor: outra forma de usar style

--- src/components/Box.vue
+++ src/components/Box.vue
@@ -1,5 +1,6 @@
+<!-- Aqui eu vou fazer o style de outra forma só para conhecimento -->
 <template>
-    <div class="box has-text-weight-bold">
+    <div class="box has-text-weight-bold" :style="estilos">
         <slot></slot> <!-- Slot é como se fosse o children -->
     </div>
 </template>
@@ -10,15 +11,26 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
     // eslint-disable-next-line vue/multi-word-component-names
-    name: 'Box'
+    name: 'Box',
+
+    data () {
+        return {
+            estilos: {
+                background: '#faf0ca'
+            }
+        }
+    }
 })
 
 </script>
 
 
+<!-- 
+
 <style scoped>
 .box{
     background: #faf0ca;
 }
+</style> 
 
-</style>
+-->
