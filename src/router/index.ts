e5ede98baf727d997eb36927c78a81c3d0ab1c95
anacperdigao <anacarolinaperdigao1@gmail.com>
2023-01-14 18:32:46 -0300

    criando o router

---
+++ src/router/index.ts
@@ -0,0 +1,20 @@
+import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
+import Tarefas from '../views/Tarefas.vue'
+
+
+const routes: RouteRecordRaw[] = [
+    {
+        path: '/',
+        name: 'Tarefas',
+        component: Tarefas
+    }
+]
+
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes: routes,
+})
+
+
+export default router;
