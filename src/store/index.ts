e3da5f58f096da8b88165e8491df2ff0198e4ca1
anacperdigao <anacarolinaperdigao1@gmail.com>
2023-01-16 00:13:07 -0300

    adicionando store-vuex no projeto

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,7 +5,8 @@
 
 import IProjeto from "@/interfaces/IProjeto";
 import { InjectionKey } from "@vue/runtime-core";
-import { createStore, Store } from "vuex";
+import { createStore, Store, useStore as vuexUseStore } from "vuex";
+import { ADICIONA_PROJETO, ALTERA_PROJETO, EXCLUIR_PROJETO } from "./type-mutations";
 
 interface Estado {
     projetos: IProjeto[]
@@ -15,19 +16,32 @@ export const key: InjectionKey<Store<Estado>> = Symbol()
 
 export const store = createStore<Estado>({
     state: {
-        projetos: [
-            {
+        projetos: []
+    },
+    mutations: {
+        [ADICIONA_PROJETO](state, nomeDoProjeto: string) {
+            const projeto = {
                 id: new Date().toISOString(),
-                nome: 'Typescript'
-            },
-            {
-                id: new Date().toISOString(),
-                nome: 'Vue'
-            },
-            {
-                id: new Date().toISOString(),
-                nome: 'Vuex'
-            }
-        ]
+                nome: nomeDoProjeto
+            } as IProjeto
+            state.projetos.push(projeto)
+        },
+
+        [ALTERA_PROJETO](state, projeto: IProjeto) {
+            const index = state.projetos.findIndex(proj => proj.id == projeto.id)
+            state.projetos[index] = projeto
+        },
+
+        [EXCLUIR_PROJETO](state, id: string) {
+            const index = state.projetos.findIndex(proj => proj.id == id)
+            state.projetos.splice(index, 1)
+        }
     }
 })
+
+// Aqui vou criar tipo um hook personalizado e só fiz isso pra facilitar na hora de usar no outros componentes,
+// pq nao preciso importar o useStore e key separados, eu ja juntei e mandei juntos
+ 
+export function useStore(): Store<Estado> {
+    return vuexUseStore(key)
+}
