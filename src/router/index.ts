1523b6ac4ddbaa40e8ec9866d112b71e745dc18d
anacperdigao <anacarolinaperdigao1@gmail.com>
2023-01-16 00:12:06 -0300

    configurando rotas aninhadas

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import Tarefas from '../views/Tarefas.vue'
 import Projetos from '../views/Projetos.vue'
+import Formulario from '../views/Projetos/Formulario.vue'
+import Lista from '../views/Projetos/Lista.vue'
 
 
 const routes: RouteRecordRaw[] = [
@@ -11,8 +13,25 @@ const routes: RouteRecordRaw[] = [
     },
     {
         path: '/projetos',
-        name: 'Projetos',
-        component: Projetos
+        component: Projetos,
+        children: [
+            {
+                path: '',
+                name: 'Projetos',
+                component: Lista
+            },
+            {
+                path: 'novo',
+                name: 'Novo projeto',
+                component: Formulario
+            },
+            {
+                path: ':id',
+                name: 'Editar projeto',
+                component: Formulario,
+                props: true
+            },
+        ]
     },
 
 ]
