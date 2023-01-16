f759c9dd6184af260331131d0218b489e9e3eb26
anacperdigao <anacarolinaperdigao1@gmail.com>
2023-01-16 00:11:26 -0300

    transformando nomes dos metodos em constantes

---
+++ src/store/type-mutations.ts
@@ -0,0 +1,5 @@
+// Aqui é só pra evitar ficar chamando os métodos por strings, então transformei em constante
+
+export const ADICIONA_PROJETO = 'ADICIONA_PROJETO'
+export const ALTERA_PROJETO = 'ALTERA_PROJETO'
+export const EXCLUIR_PROJETO = 'EXCLUIR_PROJETO'
