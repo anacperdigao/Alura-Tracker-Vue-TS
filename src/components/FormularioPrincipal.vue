163b480e984623f3370c2e5e6b304861b48af9ae
anacperdigao <anacarolinaperdigao1@gmail.com>
2023-01-16 00:11:46 -0300

    refatorando as rotas

--- src/components/FormularioPrincipal.vue
+++ src/components/FormularioPrincipal.vue
@@ -2,13 +2,27 @@
 <template>
     <div class="box formulario">
         <div class="columns">
-            <div class="column is-8" role="form" aria-label="Formulário para criação de uma nova tarefa">
+            <div class="column is-5" role="form" aria-label="Formulário para criação de uma nova tarefa">
                 <input 
                     type="text" 
                     class="input" 
                     placeholder="Qual tarefa você deseja iniciar?"
                     v-model="descricao" 
                 >
+            </div>
+            <div class="column is-3">
+                <div class="select">
+                    <select v-model="idProjeto">
+                        <option value="">Selecione o projeto</option>
+                        <option 
+                            :value="projeto.id"
+                            v-for="projeto in projetos"
+                            :key="projeto.id"
+                        >
+                            {{ projeto.nome }}
+                        </option>
+                    </select>
+                </div>
             </div>
             <div class="column">
                 <Temporizador @aoTemporizadorFinalizado="finalizarTarefa"/>
@@ -19,7 +33,9 @@
 
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { key } from '@/store';
+import { defineComponent, computed } from 'vue';
+import { useStore } from 'vuex';
 import Temporizador from './Temporizador.vue';
 
 export default defineComponent({
@@ -33,6 +49,7 @@ export default defineComponent({
     data () {
         return {
             descricao: "",
+            idProjeto: ""
         }
     },
 
@@ -43,9 +60,17 @@ export default defineComponent({
         finalizarTarefa (tempoDecorrido: number): void {
             this.$emit('aoSalvarTarefa', {
                 duracaoEmSegundos: tempoDecorrido,
-                descricao: this.descricao
+                descricao: this.descricao,
+                projeto: this.projetos.find(proj => proj.id == this.idProjeto)
             })
             this.descricao = ""
+        }
+    },
+
+    setup () {
+        const store = useStore(key)
+        return{
+            projetos: computed(() => store.state.projetos)
         }
     }
 })
--- src/views/Projetos.vue
+++ src/views/Projetos.vue
@@ -1,73 +1,17 @@
 <template>
-    <section class="projetos">
+    <div class="projetos">
         <h1 class="title">Projetos</h1>
-        <form @submit.prevent="salvar">
-            <div class="field">
-                <label for="nomeDoProjeto" class="label">
-                    Nome do Projeto
-                </label>
-                <input 
-                    type="text" 
-                    class="input" 
-                    v-model="nomeDoProjeto" 
-                    id="nomeDoProjeto" 
-                />
-            </div>
-            <div class="field">
-                <button class="button" type="submit">
-                    Salvar
-                </button>
-            </div>
-        </form>
 
-        <table class="table is-fullwidth">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Nome do Projeto</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="projeto in projetos" :key="projeto.id">
-                    <td>{{ projeto.id }}</td>
-                    <td>{{ projeto.nome }}</td>
-                </tr>
-            </tbody>
-
-        </table>
-    </section>
-
+        <router-view></router-view>
+    </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import IProjeto from '../interfaces/IProjeto'
 
-
-export default defineComponent({
-    name: 'ProjetosView',
-
-    data() {
-        return{
-            nomeDoProjeto: '',
-            projetos: [] as IProjeto[],
-        }
-    },
-
-    methods: {
-        salvar () {
-            const projeto: IProjeto = {
-                nome: this.nomeDoProjeto,
-                id: new Date().toISOString() //aqui foi só pra pegar um id generico
-            }
-
-            this.projetos.push(projeto)
-            this.nomeDoProjeto = ''
-            // Ou seja, a pessoa vai clicar em salvar, vou criar um projeto novo e armazenar na const projeto,
-            // depois vou popular o array projetos com esse projeto dando push nele, e depois vou limpar o input.
-        }
-    }
-})
+export default defineComponent ({
+    name: 'ProjetosView'
+}) 
 
 </script>
 
